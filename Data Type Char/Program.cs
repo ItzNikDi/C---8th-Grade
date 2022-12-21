fc827bfdc9bf12906b49d1748e164c1e2ec3fa63
Nik Di <nomorenikdi@gmail.com>
2022-12-21 19:16:09 +0200

    the *improved* commit

--- Data Type Char/Program.cs
+++ Data Type Char/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Data_Type_Char
+﻿using System;
+
+namespace Data_Type_Char
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+             while (true)
+             {
+                 char symbol = char.Parse(Console.ReadLine());  // could be improved by adding a check for the console return 
+                 Console.WriteLine($"The code of '{symbol}' is {(int)symbol}");  // a bit sloppy but works pretty well for the job that is has
+             }
         }
     }
 }
